aebeef5b3831213f4095759e6bf708cddec14885
John Reitano <jreitano@gmail.com>
2023-10-09 16:20:24 -0700

    fix file_store cache config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
 
   # Use a different cache store in production.
-  config.cache_store = :file_store
+  config.cache_store = :file_store, "#{root}/tmp/cache/"
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
