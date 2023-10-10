4f20dda93065a89ccf2b47ffb73c269b95d24cad
John Reitano <jreitano@gmail.com>
2023-10-09 18:23:39 -0700

    remove unused files

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -7,16 +7,8 @@ class PlacesController < ApplicationController
   # POST /places or /places.json
   def create
     @place = Place.new(place_params)
-
-    respond_to do |format|
-      if @place.retrieve_weather
-        format.html { render :index, status: :created }
-        format.json { render :show, status: :created, location: @place }
-      else
-        format.html { render :index, status: :unprocessable_entity }
-        format.json { render json: @place.errors, status: :unprocessable_entity }
-      end
-    end
+    success = @place.retrieve_weather
+    render :index, status: success ? :created : :unprocessable_entity
   end
 
   private
